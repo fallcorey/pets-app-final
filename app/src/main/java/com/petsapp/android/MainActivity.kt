26eaa30eb6e5609a8b047a1b4b79dd1dfead84a1
fallcorey <fallcorey.a@gmail.com>
2026-03-26 22:32:30 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/petsapp/android/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.petsapp.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.petsapp.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.textTitle.text = "🐾 Pets App"
+        binding.textSubtitle.text = "Добро пожаловать!\nВаши питомцы под присмотром"
+    }
+}
